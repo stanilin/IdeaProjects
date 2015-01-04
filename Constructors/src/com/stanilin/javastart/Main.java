771558a401286769a49b8a4bafb49da744b32724
stanilin <stan122002@gmail.com>
2015-01-04 22:57:12 +0500

    Added Class Main

---
+++ Constructors/src/com/stanilin/javastart/Main.java
@@ -0,0 +1,12 @@
+package com.stanilin.javastart;
+
+import com.stanilin.javastart.Object2.Object2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Object2 object2 = new Object2("blue","32");
+        object2.printProperties();
+    }
+}
