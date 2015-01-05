2c5e71aaabea1af0d6bc3f8750fa67c0254b0977
stanilin <stan122002@gmail.com>
2015-01-05 14:40:46 +0500

    In class Main I had try to create new Object House and then called it from method Main, smth like this: public class Main{     House house = new House;
        public static void main(String[] args){
        house.welcomeHouse();
        }
    }
    It doesn't work...

--- GetSet/src/com/stanilin/javastart/Main.java
+++ GetSet/src/com/stanilin/javastart/Main.java
@@ -4,7 +4,7 @@ package com.stanilin.javastart;
 import com.stanilin.javastart.House.House;
 
 public class Main {
-
+    //House house = new House(); //Вынос создания объекта на уровень класса с вызовом его в классе Main с помощью this.house.welcomeHouse() не работает
     public static void main(String[] args) {
         House house = new House();
         house.welcomeHouse();
