5646fd2beab87ee277e43ac2da26fef28173fa04
stanilin <stan122002@gmail.com>
2015-01-05 19:13:35 +0500

    I had transfered check instanseof to class Monitor. In class Main I call method monitor.connectDevice() with different variables as parameter in that method.

--- Polymorph/src/com/stanilin/javastart/Main.java
+++ Polymorph/src/com/stanilin/javastart/Main.java
@@ -4,6 +4,7 @@ import com.stanilin.javastart.comp.Computer;
 import com.stanilin.javastart.comp.Notebook;
 import com.stanilin.javastart.comp.PComputer;
 import com.stanilin.javastart.comp.TabletPC;
+import com.stanilin.javastart.monitor.Monitor;
 
 public class Main {
 
@@ -13,19 +14,20 @@ public class Main {
         Computer notebook = new Notebook("Notebook","Lenovo",4096,500);
         Computer pcComputer = new PComputer("PC","Dell",2048,750);
         Computer tablet = new TabletPC("Tablet","iPad",512,32);
+        Monitor monitor = new Monitor();
 
-        connectAll(pcComputer);
+        monitor.connectDevice(tablet);
 
-
-    }
-    public static void connectAll(Computer comp){
-        if (comp instanceof PComputer){
-            ((PComputer)comp).connect();
-        } else if (comp instanceof Notebook){
-            ((Notebook)comp).connect();
-        } else {
-            ((TabletPC) comp).connect();
-        }
 
     }
+//    public static void connectAll(Computer comp){
+//        if (comp instanceof PComputer){
+//            ((PComputer)comp).connect();
+//        } else if (comp instanceof Notebook){
+//            ((Notebook)comp).connect();
+//        } else {
+//            ((TabletPC) comp).connect();
+//        }
+
+
 }
--- Polymorph/src/com/stanilin/javastart/monitor/Monitor.java
+++ Polymorph/src/com/stanilin/javastart/monitor/Monitor.java
@@ -1,12 +1,24 @@
 package com.stanilin.javastart.monitor;
 
+import com.stanilin.javastart.comp.Computer;
+import com.stanilin.javastart.comp.Notebook;
+import com.stanilin.javastart.comp.PComputer;
+import com.stanilin.javastart.comp.TabletPC;
+
 /**
  * Created by stanilin on 05.01.2015.
  */
 public class Monitor {
 
 
-    public void connectDevice(){
+    public void connectDevice(Computer comp){
+        if (comp instanceof PComputer){
+            ((PComputer)comp).connect();
+        } else if (comp instanceof Notebook){
+            ((Notebook)comp).connect();
+        } else {
+            ((TabletPC) comp).connect();
+        }
 
     }
 }
