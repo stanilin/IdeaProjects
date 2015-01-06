ae50286e7d5f2e42545a3d2a327499d168253c3a
stanilin <stan122002@gmail.com>
2015-01-06 21:30:39 +0500

    Playing with switch statement

--- TestForAll/src/com/stanilin/javastart/Main.java
+++ TestForAll/src/com/stanilin/javastart/Main.java
@@ -2,34 +2,27 @@ package com.stanilin.javastart;
 
 public class Main {
 
+
     public static void main(String[] args) {
 
-        double doubleValue = 24.0;
-        int intValue = (int)doubleValue;
-        System.out.println("DoubleValue is: "+doubleValue);
-        doSmth(20);
+        //double doubleValue = 24.0;
+        //int intValue = (int)doubleValue;
+        //System.out.println("DoubleValue is: "+doubleValue);
+
+        doSmth();
 
     }
 
-    public static void doSmth(int var){
-        int var1 = var;
-        if(var1 > 10)
+    public static void doSmth() {
+        int i=30;
+        switch(i)
         {
-            System.out.println("Inside if");
-            if(var1 < 21)
-            { System.out.println("var1 is greater than 10 and less than 21");
-                //System.out.println("var1 = "+var);
-            }
-            else if (var1 == 20)
-            {
-                System.out.println("var1 == 20, here was a mistake");
-            }
-            else
-            {
-                System.out.println("Inside Else statement");
-            }
-            System.out.println("Outside else if");
+            case 10 : System.out.println("Case 10!");
+                break;
+            case 20 : System.out.println("Case 20!");
+                break;
+            default : System.out.println("Default Case");
+            break;
         }
-        System.out.println("var1= "+var1);
     }
 }
