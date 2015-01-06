bb0516462d2febddfd5a116d78b88021b4f19640
stanilin <stan122002@gmail.com>
2015-01-06 20:08:36 +0500

    When I use if - else statemants like this: int var = 20; if (var>10){ System.out.println("First step"); } if (var<21){ System.out.println("Second step"); } else if(var==20){ System.out.println("Third step"); } else{ System.out.println("Fourth step"); } System.out.println("Fifth step");
    I will go throw first 2 steps, but then, when I see the right condition (var==20), but there is the "else - if" statement - so I pass this step and go to the Fifth step. But, if there is no else - if, but only if, then I go throw the Third step, and then - to the Fifth step.

--- TestForAll/src/com/stanilin/javastart/Main.java
+++ TestForAll/src/com/stanilin/javastart/Main.java
@@ -7,11 +7,29 @@ public class Main {
         double doubleValue = 24.0;
         int intValue = (int)doubleValue;
         System.out.println("DoubleValue is: "+doubleValue);
-        doSmth();
+        doSmth(20);
 
     }
 
-    public static void doSmth(){
-        System.out.println("Trying to call method doSmth without keyword 'static' - so, I can't call non static method from static main()");
+    public static void doSmth(int var){
+        int var1 = var;
+        if(var1 > 10)
+        {
+            System.out.println("Inside if");
+            if(var1 < 21)
+            { System.out.println("var1 is greater than 10 and less than 21");
+                //System.out.println("var1 = "+var);
+            }
+            else if (var1 == 20)
+            {
+                System.out.println("var1 == 20, here was a mistake");
+            }
+            else
+            {
+                System.out.println("Inside Else statement");
+            }
+            System.out.println("Outside else if");
+        }
+        System.out.println("var1= "+var1);
     }
 }
