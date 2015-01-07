4ea6a4a4eed9d17aca81cb1ca0071c43cae36ff5
stanilin <stan122002@gmail.com>
2015-01-07 17:37:27 +0500

    Playing with two dimensional array

--- TestForAll/src/com/stanilin/javastart/Main.java
+++ TestForAll/src/com/stanilin/javastart/Main.java
@@ -7,9 +7,23 @@ public class Main {
 
 
 
-        int [] array = {222,444,888};
-        for (int i = 0; i < array.length; i++){
-            System.out.println(array[i]);
+        String [][] stringArray = new String[3][2];
+        stringArray[0][0] = "Stanislav";
+        stringArray[0][1] = "Kutlin";
+        stringArray[1][0] = "Alina";
+        stringArray[1][1] = "Gladkova";
+        stringArray[2][0] = "Lion";
+        stringArray[2][1] = "Kutlin";
+
+
+
+        for (int i = 0; i < stringArray.length; i++){
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int j = 0; j<stringArray[i].length; j++){
+                stringBuilder.append(stringArray[i][j]);
+
+            }
+            System.out.println(stringBuilder);
         }
         }
 }
