718cf79cb477e950d89190212a0a1774bc0f2a70
stanilin <stan122002@gmail.com>
2015-01-07 17:15:03 +0500

    Playing with arrays

--- TestForAll/src/com/stanilin/javastart/Main.java
+++ TestForAll/src/com/stanilin/javastart/Main.java
@@ -5,24 +5,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //double doubleValue = 24.0;
-        //int intValue = (int)doubleValue;
-        //System.out.println("DoubleValue is: "+doubleValue);
 
-        doSmth();
 
-    }
-
-    public static void doSmth() {
-        int i=30;
-        switch(i)
-        {
-            case 10 : System.out.println("Case 10!");
-                break;
-            case 20 : System.out.println("Case 20!");
-                break;
-            default : System.out.println("Default Case");
-            break;
+        int [] array = {222,444,888};
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
         }
-    }
+        }
 }
